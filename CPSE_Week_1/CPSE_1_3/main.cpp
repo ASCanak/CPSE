5aff21c3a80354175da3f6e7a963d2a252a00685
Ahmet Serdar Çanak <60601881+GrenadeASC@users.noreply.github.com>
2021-02-12 13:31:41 +0100

    Improved map structure

---
+++ CPSE_Week_1/CPSE_1_3/main.cpp
@@ -0,0 +1,26 @@
+#include "hwlib.hpp"
+
+extern "C" char toggle_case( char c ){
+   if(c >= 65 && c <= 90)
+      c += 32;
+   else if(c >= 97 && c <= 122)
+      c -= 32;
+   return c;
+}
+
+extern "C" void uart_put_char( char c ){
+   hwlib::cout << c;
+}
+
+extern "C" void print_asciz( const char * s );
+
+extern "C" void application();	
+
+int main( void ){	
+   namespace target = hwlib::target;   
+    
+   // wait for the PC console to start
+   hwlib::wait_ms( 2000 );
+
+   application();
+}
