c0e7b28110d44064ffe6579b44ef788233ab726c
Ahmet Serdar Çanak <60601881+ASCanak@users.noreply.github.com>
2022-04-26 00:50:45 +0200

    Add files via upload

---
+++ CPSE_Week_4/CPSE_4_3/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+
+#include "compressor.hpp" 
+#include "decompressor.hpp" 
+
+int main( void ){	
+   lz_compressor< 4096 > compressor;
+ 
+   std::ifstream f1;
+   f1.open( "wilhelmus.txt" );
+   if( ! f1.is_open()){
+      std::cerr << "input file not opened";
+      return -1;      
+   }
+   
+   std::ofstream f2;
+   f2.open( "compressed.asm" );
+   if( ! f2.is_open()){
+      std::cerr << "output file not opened";
+      return -1;      
+   }
+   
+   f2 << ".cpu cortex-m0\n.align 2\n.data\n.global compressed\n\ncompressed:\n\t.asciz \"";
+
+   compressor.compress( 
+      [ &f1 ]()-> int { auto c = f1.get(); return f1.eof() ? '\0' : c; },
+      [ &f2 ]( char c ){ f2.put( c ); }
+  );
+  
+   f1.close();
+   f2.close();
+}
